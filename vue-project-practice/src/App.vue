3abe4ba2a9f16956f17596705d276c932341e9fd
DeaHun <eogns136@gmail.com>
2026-08-11 16:41:03 +0900

    2. Computed & Watchers 실습

--- vue-project-practice/src/App.vue
+++ vue-project-practice/src/App.vue
@@ -2,6 +2,15 @@
 // 1. Reactive State
 import ReactiveRef from './components/composition/ReactiveRef.vue'
 import ReactiveReactive from './components/composition/ReactiveReactive.vue'
+// 2. Computed / Watchers
+import ComputedBasic from './components/composition/ComputedBasic.vue'
+import WatchersBasic from './components/composition/WatchersBasic.vue'
+import WatchersMulti from './components/composition/WatchersMulti.vue'
+import WatchersDeep from './components/composition/WatchersDeep.vue'
+import WatchersReactive from './components/composition/WatchersReactive.vue'
+import WatchersRefArray from './components/composition/WatchersRefArray.vue'
+import WatchersReactiveArray from './components/composition/WatchersReactiveArray.vue'
+import WatchersWatchEffect from './components/composition/WatchersWatchEffect.vue'
 
 </script>
 
@@ -11,6 +20,16 @@ import ReactiveReactive from './components/composition/ReactiveReactive.vue'
     <hr />
     <ReactiveRef />
     <ReactiveReactive />
+    <h1>2. Computed & Watchers</h1>
+    <hr />
+    <ComputedBasic />
+    <WatchersBasic />
+    <WatchersMulti />
+    <WatchersDeep />
+    <WatchersReactive />
+    <WatchersRefArray />
+    <WatchersReactiveArray />
+    <WatchersWatchEffect />
   </div>
 </template>
 
